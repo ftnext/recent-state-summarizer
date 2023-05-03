f9eeb03c57f2aa5bc4d66ffbe5c55da3cc41dc8b
ftnext <takuyafjp+develop@gmail.com>
2023-05-03 12:26:45 +0900

    [feat] Users can see summary of blog titles with omae-doyo

--- recent_state_summarizer/__main__.py
+++ recent_state_summarizer/__main__.py
@@ -1,2 +1,18 @@
+import argparse
+
+from recent_state_summarizer.fetch import fetch_titles_as_bullet_list
+from recent_state_summarizer.summarize import summarize_titles
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    return parser.parse_args()
+
+
 def main():
-    print("hello world")
+    args = parse_args()
+
+    titles = fetch_titles_as_bullet_list(args.url)
+    summary = summarize_titles(titles)
+    print(summary)
