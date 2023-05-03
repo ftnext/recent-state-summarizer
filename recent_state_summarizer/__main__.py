bba5a1e8c97bc9112ae2c59c11736a25c8ae50df
ftnext <takuyafjp+develop@gmail.com>
2023-05-03 14:41:23 +0900

    [docs] omae-douyo users can refer help

--- recent_state_summarizer/__main__.py
+++ recent_state_summarizer/__main__.py
@@ -1,12 +1,30 @@
 import argparse
+from textwrap import dedent
 
 from recent_state_summarizer.fetch import fetch_titles_as_bullet_list
 from recent_state_summarizer.summarize import summarize_titles
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
+    help_message = """
+    Summarize blog article titles with the OpenAI API.
+
+    ⚠️ Set `OPENAI_API_KEY` environment variable.
+
+    Example:
+        omae-douyo https://awesome.hatenablog.com/archive/2023
+
+    Retrieve the titles of articles from a specified URL.
+    After summarization, prints the summary.
+
+    Support:
+        - はてなブログ（Hatena blog）
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=dedent(help_message),
+    )
+    parser.add_argument("url", help="URL of archive page")
     return parser.parse_args()
 
 
